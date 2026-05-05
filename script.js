e20240620fb27558c8ffe353e29738a1c589144a
BoaDOMO <132649911+BoaDOMO@users.noreply.github.com>
2026-05-05 21:42:12 +0700

    style: change neural links color to black

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
       else if (this.y < 0) this.y = canvas.height;
     }
     draw() {
-      ctx.fillStyle = "#185FA5";
+      ctx.fillStyle = "#000000";
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill();
@@ -204,7 +204,7 @@ document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < connectionDistance) {
-          ctx.strokeStyle = `rgba(24, 95, 165, ${1 - distance / connectionDistance})`;
+          ctx.strokeStyle = `rgba(0, 0, 0, ${1 - distance / connectionDistance})`;
           ctx.lineWidth = 0.5;
           ctx.beginPath();
           ctx.moveTo(p.x, p.y);
